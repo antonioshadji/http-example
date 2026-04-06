2b37176c59f7b3da452153a3712b1fe0b383e110
Antonios Hadjigeorgalis <ahadjigeorgalis@anaconda.com>
2026-04-06 18:02:36 -0400

    refactor: NewHandler accepts html []byte parameter

--- server/server.go
+++ server/server.go
@@ -1,15 +1,14 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func NewHandler() http.Handler {
+func NewHandler(html []byte) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprint(w, "<!DOCTYPE html><html><head><title>http-example</title></head><body><h1>I'm running on your machine</h1></body></html>")
+		w.Write(html)
 	})
 	return mux
 }
