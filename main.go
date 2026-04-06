18dddb31d3954f3820f15aa0cb24c9915c251899
Antonios Hadjigeorgalis <ahadjigeorgalis@anaconda.com>
2026-04-06 17:02:55 -0400

    feat: add main with port retry logic (8080-8089)

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/antonioshadji/http-example/server"
+)
+
+func main() {
+	handler := server.NewHandler()
+
+	const basePort = 8080
+	const maxAttempts = 10
+
+	for i := range maxAttempts {
+		port := basePort + i
+		addr := fmt.Sprintf(":%d", port)
+
+		listener, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.Printf("port %d unavailable: %v", port, err)
+			continue
+		}
+
+		log.Printf("listening on http://localhost:%d", port)
+		if err := http.Serve(listener, handler); err != nil {
+			log.Fatalf("server error: %v", err)
+		}
+		return
+	}
+
+	log.Fatalf("failed to bind to any port in range %d-%d", basePort, basePort+maxAttempts-1)
+}
